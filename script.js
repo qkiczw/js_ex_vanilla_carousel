ed7b50dc454136d5603f3d50a9961b55135f3302
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-09-14 21:05:26 +0200

    Some Js variables were added to grab html DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+const slidesContainer = document.querySelector(".carousel__slides");
+const slides = Array.from(slidesContainer.children);
+const carouselNav = document.querySelector(".carousel__nav");
+const singleSlideWidth = slides[0].getBoundingClientRect();
+const carouselDots = carouselNav.children;
+const BtnNext = document.querySelector(".carousel__button--right");
+const BtnPrev = document.querySelector(".carousel__button--left");

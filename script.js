8345bf636ed9fb86750fe51d9debf70ed69bd545
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2021-09-15 18:23:45 +0200

    Function to show next slide was added

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ console.log("Hello world!");
 const slidesContainer = document.querySelector(".carousel__slides");
 const slides = Array.from(slidesContainer.children);
 const carouselNav = document.querySelector(".carousel__nav");
-const singleSlideWidth = slides[0].getBoundingClientRect();
+const singleSlideWidth = slides[0].getBoundingClientRect().width;
 const carouselDots = carouselNav.children;
-const BtnNext = document.querySelector(".carousel__button--right");
-const BtnPrev = document.querySelector(".carousel__button--left");
+const btnNext = document.querySelector(".carousel__button--right");
+const btnPrev = document.querySelector(".carousel__button--left");
+
+const setSlidesLeftPos = (slide, index) =>
+  (slide.style.left = `${singleSlideWidth * index}px`);
+
+slides.forEach(setSlidesLeftPos);
+
+btnNext.addEventListener("click", () => {
+  const currentSlide = slidesContainer.querySelector(".current-slide");
+
+  currentSlide.nextElementSibling.style.left = 0;
+
+  currentSlide.classList.remove("current-slide");
+  currentSlide.nextElementSibling.classList.add("current-slide");
+});
+
+// Future adds:
+// - add dots base on amount of slides
